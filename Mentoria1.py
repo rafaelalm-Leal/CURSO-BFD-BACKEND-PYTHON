02e4a18ee28167af7f8a8e3c5544882620d1a6db
Rafael Almeida <rafaelalm2004@gmail.com>
2025-08-25 15:26:57 -0300

    Add files via upload
    Atividade da média de notas.

---
+++ Mentoria1.py
@@ -0,0 +1,5 @@
+nota1= float ( input ("Digite a nota 1:"))
+nota2= float(input("Digite a nota 2:"))
+nota3= float(input("Digite a nota 3:"))
+media= (nota1+nota2+nota3)/3
+print ("A média das suas notas é de: %.2f" % (media))
